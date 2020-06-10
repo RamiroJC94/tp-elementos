554d0d5d3e1b2491cd90091e9bbe7101deac49fa
miguel angel <miguelequilea18@gmail.com>
2020-06-09 21:24:10 -0300

    not found

---
+++ frontend/src/components/notFound.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import travolta from '../imagen/travolta.gif'
+import '../styles/notFound.css'
+import {withRouter}from 'react-router-dom';
+ class NotFoundPage extends React.Component{
+    render(){
+        return (
+          <div className="notFound">
+            <div>
+                <img src={travolta} alt="travolta gif" />
+            </div>
+            <div >
+                <h1 className="not">404 Page Not Found</h1>
+                <Link id="irInicio" to="/"> Ir al inicio </Link>
+            </div>
+          </div>
+        );
+    }
+}
+export default withRouter(NotFoundPage)
