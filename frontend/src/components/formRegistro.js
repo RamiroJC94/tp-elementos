4ec6c486d185289ca90cfb9641927a69111f5466
ordenador <rama.croc@gmail.com>
2020-06-16 17:37:34 -0300

    -Modify Name & component registro- - component registro to Name formRegistro - - add form - -origin rama-

---
+++ frontend/src/components/formRegistro.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {withRouter} from 'react-router-dom'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+render(){
+    return(
+        <div>
+            <Form>
+
+              <Form.Group as={Row} controlId="formPlaintextEmail">
+                <Form.Label column sm="2">
+                  Email
+                </Form.Label>
+                <Col sm="10">
+                  <Form.Control plaintext readOnly defaultValue="email@example.com" />
+                </Col>
+              </Form.Group>
+
+              <Form.Group as={Row} controlId="formPlaintextPassword">
+                <Form.Label column sm="2">
+                  Password
+                </Form.Label>
+                <Col sm="10">
+                  <Form.Control type="password" placeholder="Password" />
+                </Col>
+              </Form.Group>
+
+              <Button  variant="primary" onClick={this.logear}>
+                              Login
+              </Button>
+
+            </Form>
+        </div>
+    )
+}
+
+export default withRouter(formRegistro)
