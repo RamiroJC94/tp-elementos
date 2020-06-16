4a6446ebff24e2856325e127b6e5366103f8c769
ordenador <rama.croc@gmail.com>
2020-06-16 17:27:00 -0300

    -Create component registro- -origin rama-

---
+++ frontend/src/components/registro.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {withRouter} from 'react-router-dom'
+
+render(){
+    return(
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+    )
+}
+
+export default withRouter(registro)
