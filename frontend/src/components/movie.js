b72122483cafaa10d9a9f0e8c23678099e71133a
rama <rama.croc@gmail.com>
2020-07-01 17:53:20 -0300

    2020 07 01 us(S4HistorialPeliculas) Add Function HandlerHistoryMovie

--- frontend/src/components/movie.js
+++ frontend/src/components/movie.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import  '../styles/movie.css'
 import {Link} from 'react-router-dom'
+import api from '../api/api';
 
 export default class Movie extends React.Component{
     constructor(props){
@@ -11,15 +12,15 @@ export default class Movie extends React.Component{
         }
     }
 
-    // HistoryMovie=()=>{
-    //     let body = {titulo: this.state.titulo, imagen: this.state.imagen, trailer: this.state.trailer}
-    //      api.createMovie(body)
-    //     .then(data=>{this.props.history.push("/")})
-    //     .catch(error=>this.setState({checkEnvio:this.errorLog("algo mal anda capo")}))
-    // }
+    HandlerHistoryMovie=()=>{
+        let body = {titulo: this.state.titulo, imagen: this.state.imagen, trailer: this.state.trailer}
+         api.changeHistoryMovie(body)
+        .then(data=>{this.state.setM(this.state.peli)})
+        .catch(error=>this.setState({checkEnvio:this.errorLog("algo mal anda capo")}))
+    }
 
     render(){
         const dir="/player/"+this.state.peli.titulo
-        return (<Link to={dir} onClick={()=> this.state.setM(this.state.peli)}><img className="movie"  src={this.state.peli.imagen}></img></Link>);
+        return (<Link to={dir} onClick={this.HandlerHistoryMovie}><img className="movie"  src={this.state.peli.imagen}></img></Link>);
     }
 }
