e067df8648bc76f83b8f1f7fadb2603c6ab67b5e
rama <rama.croc@gmail.com>
2020-07-01 16:34:28 -0300

    2020 07 01 us(S4HistorialPeliculas) Add HistorySchema in user.js

--- backend/mongoose/user.js
+++ backend/mongoose/user.js
@@ -1,12 +1,16 @@
 import mongoose from 'mongoose';
-
+let HistorySchema = new mongoose.Schema({
+    titulo:{type:String},
+    imagen:{type:String},
+    trailer:{type:String} 
+})
 
 let UserSchema= new mongoose.Schema({
     mail: {type:String},
     username: {type:String},     
     password: {type:String},
     isAdmin: {type:Boolean},
-    History: {type:String},
+    History: {type:[HistorySchema]},
     });
 
 export var User = mongoose.model('User',UserSchema);         
